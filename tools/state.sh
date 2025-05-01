d3dc2d6d0c64ecaed13411e9667cd0a1c0e59080
Henrique Santos <henriquemarloncs@gmail.com>
2025-05-01 16:00:23 +0100

    chore: update state script

--- tools/state.sh
+++ tools/state.sh
@@ -15,7 +15,7 @@ stringToHex() {
 
 # Ethereum addresses
 INPUT_BOX="0xB6b39Fb3dD926A9e3FBc7A129540eEbeA3016a6c"
-DAPP_ADDRESS="0x722b795a74b8a25d170a5f2fe90dfe81ebacc06a"
+DAPP_ADDRESS="0x047b88c0fed7e2ce3848cc22dd3406d6de2454dd"
 PORTAL_ADDRESS="0x05355c2F9bA566c06199DEb17212c3B78C1A3C31"
 ADMIN_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 ADMIN_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
@@ -38,22 +38,19 @@ INVESTOR_PRIVATE_KEYS=(
     "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"
 )
 
+SLEEP_TIME=${SLEEP_TIME:-5}
+
 # Deploy the Token contract and capture the deployed address
 deployToken() {
     local tokenName="$1"
     local tokenSymbol="$2"    
-    # Execute the forge create command and capture the output
     result=$(forge create ./src/Token.sol:Token \
         --private-key $ADMIN_PRIVATE_KEY \
         --rpc-url http://localhost:8080/anvil \
         --root ./contracts \
         --broadcast \
         --constructor-args "$tokenName" "$tokenSymbol" 2>&1)
-    
-    # Extract the deployed address from the output
     deployedAddress=$(echo "$result" | grep "Deployed to:" | awk '{print $3}')
-    
-    # Check if the deployed address was extracted
     if [[ -z "$deployedAddress" ]]; then
         echo "Error: Failed to deploy contract for $tokenName ($tokenSymbol)."
         echo "$result"
@@ -66,7 +63,7 @@ deployToken() {
 sendInput() {
     local payload="$1"
     hexPayload=$(stringToHex "$payload")
-    cast send $INPUT_BOX "addInput(address,bytes)(bytes32)" $DAPP_ADDRESS $hexPayload --private-key $ADMIN_PRIVATE_KEY --rpc-url http://localhost:8080/anvil
+    cast send $INPUT_BOX "addInput(address,bytes)(bytes32)" $DAPP_ADDRESS $hexPayload --private-key $ADMIN_PRIVATE_KEY --priority-gas-price 10000 --rpc-url http://localhost:8080/anvil $GAS_FLAG
 }
 
 # Mint tokens to a specified address
@@ -74,7 +71,7 @@ mintTokens() {
     local tokenAddress="$1"
     local recipient="$2"
     local amount="$3"
-    cast send $tokenAddress "mint(address,uint256)" $recipient $amount --private-key $ADMIN_PRIVATE_KEY --rpc-url http://localhost:8080/anvil
+    cast send $tokenAddress "mint(address,uint256)" $recipient $amount --private-key $ADMIN_PRIVATE_KEY --priority-gas-price 10000 --rpc-url http://localhost:8080/anvil $GAS_FLAG
     echo "Minted $amount tokens to $recipient on $tokenAddress"
 }
 
@@ -89,7 +86,8 @@ approveTokens() {
         "approve(address,uint256)" \
         $spender $amount \
         --private-key $privateKey \
-        --rpc-url http://localhost:8080/anvil
+        --priority-gas-price 10000 \
+        --rpc-url http://localhost:8080/anvil $GAS_FLAG
 }
 
 # Function to deposit ERC20 tokens
@@ -104,15 +102,16 @@ depositERC20Tokens() {
         "depositERC20Tokens(address,address,uint256,bytes)" \
         $token $dapp $amount "$(stringToHex $execLayerData)" \
         --private-key $privateKey \
-        --rpc-url http://localhost:8080/anvil
+        --priority-gas-price 10000 \
+        --rpc-url http://localhost:8080/anvil $GAS_FLAG
 }
 
 echo "Deploying contracts..."
 STABLECOIN_ADDRESS=$(deployToken "Stablecoin" "STABLECOIN")
-sleep 1
+sleep $SLEEP_TIME
 
 TOKENIZED_RECEIVABLE_ADDRESS=$(deployToken "Pink" "PINK")
-sleep 1
+sleep $SLEEP_TIME
 
 echo "Deployed contracts:"
 echo "STABLECOIN_ADDRESS=$STABLECOIN_ADDRESS"
@@ -120,85 +119,107 @@ echo "TOKENIZED_RECEIVABLE_ADDRESS=$TOKENIZED_RECEIVABLE_ADDRESS"
 
 echo "Minting tokens to investors and creator..."
 mintTokens $TOKENIZED_RECEIVABLE_ADDRESS $CREATOR_ADDRESS 10000000
-sleep 1
+sleep $SLEEP_TIME
 
 mintTokens $STABLECOIN_ADDRESS $CREATOR_ADDRESS 10000000
-sleep 1
+sleep $SLEEP_TIME
 
-for investor in "${INVESTOR_ADDRESSES[@]}"; do
-    mintTokens $STABLECOIN_ADDRESS $investor 10000000
-    sleep 1
-done
+mintTokens $STABLECOIN_ADDRESS ${INVESTOR_ADDRESSES[0]} 10000000
+sleep $SLEEP_TIME
+mintTokens $STABLECOIN_ADDRESS ${INVESTOR_ADDRESSES[1]} 10000000
+sleep $SLEEP_TIME
+mintTokens $STABLECOIN_ADDRESS ${INVESTOR_ADDRESSES[2]} 10000000
+sleep $SLEEP_TIME
+mintTokens $STABLECOIN_ADDRESS ${INVESTOR_ADDRESSES[3]} 10000000
+sleep $SLEEP_TIME
+mintTokens $STABLECOIN_ADDRESS ${INVESTOR_ADDRESSES[4]} 10000000
+sleep $SLEEP_TIME
 
 # Create contracts
 echo "Creating contracts..."
 sendInput '{"path":"create_contract","payload":{"symbol":"STABLECOIN","address":"'"$STABLECOIN_ADDRESS"'"}}'
-sleep 1
+sleep $SLEEP_TIME
 sendInput '{"path":"create_contract","payload":{"symbol":"TOKENIZED_RECEIVABLE","address":"'"$TOKENIZED_RECEIVABLE_ADDRESS"'"}}'
-sleep 1
+sleep $SLEEP_TIME
 
 # Create users
 echo "Creating users..."
 sendInput '{"path":"create_user","payload":{"address":"'"$CREATOR_ADDRESS"'","role":"creator"}}'
-sleep 1
-for i in "${!INVESTOR_ADDRESSES[@]}"; do
-    role="non_qualified_investor"
-    [ "$i" -lt 2 ] && role="qualified_investor"
-    sendInput '{"path":"create_user","payload":{"address":"'"${INVESTOR_ADDRESSES[$i]}"'","role":"'"$role"'"}}'
-    sleep 1
-done
+sleep $SLEEP_TIME
+sendInput '{"path":"create_user","payload":{"address":"'${INVESTOR_ADDRESSES[0]}'","role":"qualified_investor"}}'
+sleep $SLEEP_TIME
+sendInput '{"path":"create_user","payload":{"address":"'${INVESTOR_ADDRESSES[1]}'","role":"qualified_investor"}}'
+sleep $SLEEP_TIME
+sendInput '{"path":"create_user","payload":{"address":"'${INVESTOR_ADDRESSES[2]}'","role":"non_qualified_investor"}}'
+sleep $SLEEP_TIME
+sendInput '{"path":"create_user","payload":{"address":"'${INVESTOR_ADDRESSES[3]}'","role":"non_qualified_investor"}}'
+sleep $SLEEP_TIME
+sendInput '{"path":"create_user","payload":{"address":"'${INVESTOR_ADDRESSES[4]}'","role":"non_qualified_investor"}}'
+sleep $SLEEP_TIME
 
 # Create crowdfunding
 echo "Creating crowdfunding..."
 current_timestamp=$(date +%s)
-closes_at=$((current_timestamp + 60))
-maturity_at=$((current_timestamp + 120))
+closes_at=$((current_timestamp + 80))
+maturity_at=$((current_timestamp + 150))
 crowdfundingPayload='{"path":"create_crowdfunding","payload":{"max_interest_rate":"10","debt_issued":"100000","fundraising_duration":50,"closes_at":'"$closes_at"',"maturity_at":'"$maturity_at"'}}'
 approveTokens $TOKENIZED_RECEIVABLE_ADDRESS $PORTAL_ADDRESS 10000 $CREATOR_PRIVATE_KEY
-sleep 1
+sleep $SLEEP_TIME # +5s
 depositERC20Tokens $TOKENIZED_RECEIVABLE_ADDRESS $DAPP_ADDRESS 10000 "$crowdfundingPayload" $CREATOR_PRIVATE_KEY
-sleep 1
+sleep $SLEEP_TIME # +5s
 
+# 4. Update crowdfunding to ongoing (sent by admin)
+echo "Updating crowdfunding state to 'ongoing'..."
+updatePayload='{"path":"update_crowdfunding","payload":{"id":1,"state":"ongoing"}}'
+sendInput "$updatePayload" $ADMIN_PRIVATE_KEY
 
-# # 4. Update crowdfunding to ongoing (sent by admin)
-# echo "Updating crowdfunding state to 'ongoing'..."
-# updatePayload='{"path":"update_crowdfunding","payload":{"id":1,"state":"ongoing"}}'
-# sendInput "$updatePayload" $ADMIN_PRIVATE_KEY &
-# sleep 11
-# wait
+# 5. Create orders from investors (sent by each investor)
+echo "Creating orders from investors..."
+ORDER_AMOUNTS=(60000 52000 2000 3000 400)
+INTEREST_RATES=("9" "8" "4" "6" "4")
 
-# # 5. Create orders from investors (sent by each investor)
-# echo "Creating orders from investors..."
-# ORDER_AMOUNTS=(60000 52000 2000 3000 400)
-# INTEREST_RATES=("9" "8" "4" "6" "4")
-# for i in "${!INVESTOR_ADDRESSES[@]}"; do
-#     orderPayload='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"'"${INTEREST_RATES[$i]}"'"}}'
-#     approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS "${ORDER_AMOUNTS[$i]}" "${INVESTOR_PRIVATE_KEYS[$i]}" &
-#     sleep 1
-#     depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS "${ORDER_AMOUNTS[$i]}" "$orderPayload" "${INVESTOR_PRIVATE_KEYS[$i]}" &
-#     sleep 1
-# done
-# sleep 1
-# wait
+orderPayload0='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"9"}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 60000 ${INVESTOR_PRIVATE_KEYS[0]} &
+sleep $SLEEP_TIME # +5s
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 60000 "$orderPayload0" ${INVESTOR_PRIVATE_KEYS[0]} &
+sleep $SLEEP_TIME # +5s
 
-# # 6. Wait for crowdfunding close
-# echo "Waiting for crowdfunding to expire..."
-# sleep 60
+orderPayload1='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"8"}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 52000 ${INVESTOR_PRIVATE_KEYS[1]} &
+sleep $SLEEP_TIME # +5s
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 52000 "$orderPayload1" ${INVESTOR_PRIVATE_KEYS[1]} &
+sleep $SLEEP_TIME # +5s
 
-# # 7. Close crowdfunding (sent by admin)
-# echo "Closing crowdfunding..."
-# closePayload='{"path":"close_crowdfunding","payload":{"creator":"'"$CREATOR_ADDRESS"'"}}'
-# sendInput "$closePayload" $ADMIN_PRIVATE_KEY &
-# wait
+orderPayload2='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"4"}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 2000 ${INVESTOR_PRIVATE_KEYS[2]} &
+sleep $SLEEP_TIME # +5s
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 2000 "$orderPayload2" ${INVESTOR_PRIVATE_KEYS[2]} &
+sleep $SLEEP_TIME # +5s
 
-# # 8. Wait for crowdfunding maturity
-# echo "Waiting for crowdfunding maturity..."
-# sleep 30
+orderPayload3='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"6"}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 3000 ${INVESTOR_PRIVATE_KEYS[3]} &
+sleep $SLEEP_TIME # +5s
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 3000 "$orderPayload3" ${INVESTOR_PRIVATE_KEYS[3]} &
+sleep $SLEEP_TIME # +5s
 
-# # 9. Settle crowdfunding (sent by creator using stablecoin)
-# echo "Settling crowdfunding..."
-# settlePayload='{"path":"settle_crowdfunding","payload":{"crowdfunding_id":1}}'
-# approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 108270 $CREATOR_PRIVATE_KEY &
-# sleep 1
-# depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 108270 "$settlePayload" $CREATOR_PRIVATE_KEY &
-# wait
+orderPayload4='{"path":"create_order","payload":{"crowdfunding_id":1,"interest_rate":"4"}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 400 ${INVESTOR_PRIVATE_KEYS[4]} &
+sleep $SLEEP_TIME # +5s
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 400 "$orderPayload4" ${INVESTOR_PRIVATE_KEYS[4]} &
+sleep $SLEEP_TIME # +5s
+
+sleep 40
+wait
+
+# 7. Close crowdfunding (sent by admin)
+echo "Closing crowdfunding..."
+closePayload='{"path":"close_crowdfunding","payload":{"creator":"'"$CREATOR_ADDRESS"'"}}'
+sendInput "$closePayload" $ADMIN_PRIVATE_KEY
+
+# 9. Settle crowdfunding (sent by creator using stablecoin)
+echo "Settling crowdfunding..."
+settlePayload='{"path":"settle_crowdfunding","payload":{"crowdfunding_id":1}}'
+approveTokens $STABLECOIN_ADDRESS $PORTAL_ADDRESS 108270 $CREATOR_PRIVATE_KEY &
+sleep $SLEEP_TIME
+depositERC20Tokens $STABLECOIN_ADDRESS $DAPP_ADDRESS 108270 "$settlePayload" $CREATOR_PRIVATE_KEY &
+wait
