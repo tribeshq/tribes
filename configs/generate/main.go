dab5130b5b47d316b9641082b2d7c492af32166c
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-15 18:51:55 +0100

    chore: some changes

--- configs/generate/main.go
+++ configs/generate/main.go
@@ -10,7 +10,7 @@
 package main
 
 func main() {
-	data := readTOML("../../rollup.toml")
+	data := readTOML("./config.toml")
 	config := decodeTOML(data)
 	envs := sortConfig(config)
 	for _, env := range envs {
